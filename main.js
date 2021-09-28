2a2a32c3790ccbe222dc22e3387f209e24e8b907
NoDisassemble <anything@dant35an0nym0u5.com>
2021-09-28 14:49:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ let genreToggle = document.getElementById("form-by-genre");
 let actorToggle = document.getElementById("actor-search");
 let directorToggle = document.getElementById("director-search");
 
-let title = document.getElementById("returnMovie");
-let overview = document.getElementById("returnOverview");
-
 
 const resettableElements = [
     genreToggle,
@@ -16,7 +13,6 @@ function toggleElement(element) {
     resettableElements.forEach((node) => {
         node.style.display = "none";
     });
-
     element.style.display = "block";
 }
 
@@ -38,27 +34,65 @@ function ShowHideDirector() {
     toggleElement(directorToggle);
 }
 
-// Random number generator for movie ID
+// ------------------------ Fetch by Genre
+// Grab id from selected option
+function getGenre(s) {
+    genreId = (s[s.selectedIndex].id);
+    page = Math.floor(Math.random() * 500) + 1;
+    console.log("Genre: " + document.getElementById("genre-option").value);
+    console.log("Genre Id: " + s[s.selectedIndex].id);
+    console.log("Page: " + page);
+    url = `https://api.themoviedb.org/3/discover/movie?api_key=e4fc096b1f7f3fbf48013349c7456fef&language=en-US&page=${page}&include_adult=false&with_genres=${genreId}`;
+}
+
+
+// ------------------------ Fecth by Actor
+
+
+// ------------------------ Fetch by Director
+
+
+// ------------------------ Surprise Me
+// Generate random id
 function SurpriseMe() {
     let surpriseToggle = document.getElementById("surprise-me");
-    toggleElement(surpriseToggle);
     randomId = Math.floor(Math.random() * 800000) + 1;
-    console.log(randomId);
+    toggleElement(surpriseToggle);
+    console.log("Movie Id: " + randomId);
+    url = `https://api.themoviedb.org/3/movie/${randomId}?api_key=e4fc096b1f7f3fbf48013349c7456fef`;
+    return randomId;
 }
 
 // Fetch movie from TMDB API
 async function FindMovie() {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${randomId}?api_key=e4fc096b1f7f3fbf48013349c7456fef`);
-    const data = await response.json();
-    if (data && data.success === false) {
-        return randomId;
-    } else {
-        title = data.title;
-        overview = data.overview;
-        console.log(data);
-        title = document.getElementById("returnMovie").innerHTML = title;
-        overview = document.getElementById("returnOverview").innerHTML = overview;
-        console.log(title);
-        return title;
+    let response = await fetch(url);
+    let data = await response.json();
+    if (!response.ok) {
+        SurpriseMe();
+        FindMovie();
     }
+    title = data.title;
+    overview = data.overview;
+    if (overview === "") {
+        overview = "Description Unavailable";
+    }
+    genre = data.genres.map(genre => (" " + genre.name));
+    if (genre.length === 0) {
+        genre = "Unavailable";
+    }
+    year = data.release_date;
+    if (year === "") {
+        year = "Unavailable";
+    }
+    title = document.getElementById("returnMovie").innerHTML = title;
+    overview = document.getElementById("returnOverview").innerHTML = overview;
+    genre = document.getElementById("returnGenre").innerHTML = genre;
+    year = document.getElementById("returnYear").innerHTML = year;
+    console.log(data);
+    console.log("Movie Id: " + randomId)
+    console.log("Title: " + title);
+    console.log("Description: " + overview);
+    console.log("Genres: " + data.genres.map(genre => (" " + genre.name)));
+    console.log("Release Date: " + year);
+
 }
